88909053c5ed87618003e48e8f8e8706aa622c2c
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-01-21 17:41:39 -0800

    added javascript and interactive questions

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+let name = prompt("What is your name?");
+alert("Welcome Back! " + name );
+
+let amountOfDogs = prompt("How many dogs do you want to adopt");
+alert("Wow! Thanks for adopting " + amountOfDogs + " dogs!"); 
+
+let dogSex = prompt("Are you looking for a male or female dog?");
+if (dogSex==="male") {
+alert("Great! We have 20 male dogs waiting to be adopted.");
+}
+else if (dogSex==="female") {
+alert("Great! We have 62 female dogs waiting to be adopted.");
+}
+else {
+alert("Oops! That wasn't an option!");
+}

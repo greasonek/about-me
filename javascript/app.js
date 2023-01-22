f3859a139d514cc40b1ebafc86541eeed196ec1f
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-01-21 17:50:05 -0800

    edited js link in index

--- javascript/app.js
+++ javascript/app.js
@@ -6,11 +6,13 @@ alert("Wow! Thanks for adopting " + amountOfDogs + " dogs!");
 
 let dogSex = prompt("Are you looking for a male or female dog?");
 if (dogSex==="male") {
-alert("Great! We have 20 male dogs waiting to be adopted.");
+    alert("Great! We have 20 male dogs waiting to be adopted.");
 }
+
 else if (dogSex==="female") {
-alert("Great! We have 62 female dogs waiting to be adopted.");
+    alert("Great! We have 62 female dogs waiting to be adopted.");
 }
+
 else {
-alert("Oops! That wasn't an option!");
+    alert("Oops! That wasn't an option!");
 }

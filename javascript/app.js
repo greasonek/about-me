764de1461fb49de174e89234a145db1bba462085
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2023-01-24 21:20:06 -0800

    added while loop into js

--- javascript/app.js
+++ javascript/app.js
@@ -1,27 +1,43 @@
-let name = prompt("What is your name?");
-alert("Welcome Back! " + name );
+let name = prompt("Welcome to Love Dogs Adoption! What is your name?");
+alert("We're glad you're here " + name + "! We just have a couple of questions.");
+
+// function adoptDogs() {
+//    let amountOfDogs = prompt("How many dogs do you want to adopt");
+//   amountOfDogs = Number(amountOfDogs);
+//    if (amountOfDogs < 3) {
+//       let adoptDogs = prompt ("C'mon you can adopt more than that!");
+//        if (adoptDogs >= 3) {
+//        document.write("Wow! Thanks for adopting " + adoptDogs + "dogs!")
+//    }
+//    } else {s
+//        let message= "Wow! Thanks for adopting " + amountOfDogs + "dogs!";
+//        document.write(message);
+//    }
+//}
 
 function adoptDogs() {
-    let amountOfDogs = prompt("How many dogs do you want to adopt");
-    amountOfDogs = Number(amountOfDogs);
-    if (amountOfDogs < 3) {
-        let adoptDogs = prompt ("C'mon you can adopt more than that!");
-        if (adoptDogs >= 3) {
-        document.write("Wow! Thanks for adopting " + adoptDogs + "dogs!")
+
+    let adopt = prompt("How many dogs do you want to adopt?");
+
+    while (adopt < 4) {
+        adopt = prompt ("C'mon you can adopt more than that!");
+        adopt = Number(adopt);
     }
-    } else {
-        let message= "Wow! Thanks for adopting " + amountOfDogs + "dogs!";
-        document.write(message);
+    for (let i = 1; i<= adopt; i++) {
+        document.write("🐶");
     }
 }
+
+
+
 
 let dogSex = prompt("Are you looking for a male or female dog?");
 if (dogSex==="male") {
-    alert("Great! We have 20 male dogs waiting to be adopted.");
+    alert("Great! We have 28 male dogs waiting to be adopted.");
 }
 
 else if (dogSex==="female") {
-    alert("Great! We have 62 female dogs waiting to be adopted.");
+    alert("Great! We have 32 female dogs waiting to be adopted.");
 }
 
 else {
